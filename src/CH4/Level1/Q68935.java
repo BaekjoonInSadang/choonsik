6cf80371cdc486e6325f91a651d8dc7092ae36eb
Choonsik <101438447+moodeary@users.noreply.github.com>
2024-07-18 18:30:25 +0900

    Chapter4 Level1 Clear

--- src/CH4/Level1/Q68935.java
+++ src/CH4/Level1/Q68935.java
@@ -1,11 +1,50 @@
 package CH4.Level1;
 
 /**
- * 3진법 뒤집기
+ * ==========================================================
+ * [제목] 3진법 뒤집기
+ * ==========================================================
+ * [문제]
+ * 자연수 n이 매개변수로 주어집니다.
+ * n을 3진법 상에서 앞뒤로 뒤집은 후
+ * 다시 10진법으로 표현한 수를 return 제출
+ * ==========================================================
+ * [제한 사항]
+ * 1 <= n <= 100,000,000 자연수
+ * ==========================================================
+ * [풀이]
+ * 3진법 변환 메서드 생성
+ * 주어진 값을 3진법으로 바꾸고 뒤집고 10진법으로 변환 끝
  */
 public class Q68935 {
+
+    public static void main(String[] args) {
+        Q68935 q68935 = new Q68935();
+        int result = q68935.solution(125);
+
+        System.out.println("result = " + result);
+    }
+
     public int solution(int n) {
-        int answer = 0;
-        return answer;
+
+        // 3진법으로 변환
+        String ternary = toTernary(n);
+
+        // 10진법으로 변환
+        return Integer.parseInt(ternary, 3);
+    }
+
+    // 10진수를 3진수로 변환
+    public static String toTernary(int number) {
+        if (number == 0) return "0";
+
+        StringBuilder ternary = new StringBuilder();
+        while (number > 0) {
+            int remainder = number % 3;
+            ternary.insert(0, remainder);
+            number /= 3;
+        }
+        // 뒤집어!
+        return ternary.reverse().toString();
     }
 }
